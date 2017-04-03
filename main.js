18be18cb0f4df614c40f861a9e6e10b65dada261
Srikanth Mandava <mandavavenkatasrikanth@gmail.com>
2017-04-02 21:38:26 -0400

    Add addition function

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ function myMessage() {
 }
 
 myMessage();
+
+/* sum of two numbers */
+function addition(a,b){
+  return a+b;
+}
+
+
+
+var addOutput = document.getElementById('addition-output');
+addOutput.innerHTML = addition(3,4);
