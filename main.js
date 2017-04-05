b9b696e0d19561ba6f41bf91004ef4816043095a
Srikanth Mandava <mandavavenkatasrikanth@gmail.com>
2017-04-05 17:39:01 -0400

    Added Multiplication function

--- main.js
+++ main.js
@@ -13,7 +13,15 @@ function addition(a,b){
   return a+b;
 }
 
+/*Multiplication of two numbers*/
+function multiply(a,b){
+  return a*b;
+}
+
 
 
 var addOutput = document.getElementById('addition-output');
 addOutput.innerHTML = addition(3,4);
+
+var multiplyOutput = document.getElementById('multiply-output');
+multiplyOutput.innerHTML = multiply(5,5);
